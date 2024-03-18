7db376d89f69971768058065fe6c8f82a3b7b041
leeyeoreum02 <leeyeoreum01@gmail.com>
2024-03-18 02:57:26 +0000

    3/13 add loss visualization

--- tests/test_visualize_height_matrix.py
+++ tests/test_visualize_height_matrix.py
@@ -6,9 +6,9 @@ from compute_lmc import visualize_conf_matrix
 
 def test_visualize_lmc():
     json_root_dir = 'lmc_data'
-    dataset_name = 'ruin_names'
+    dataset_name = 'geometric_shapes'
     visualize_dir = 'visualize'
-    json_path = os.path.join(json_root_dir, dataset_name, 'acc_list_seed1,2,3,4,5.json')
+    json_path = os.path.join(json_root_dir, dataset_name, 'loss_list_seed1,2,3,4,5.json')
 
     visualize_conf_matrix(
         json_path=json_path,
